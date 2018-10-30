5a6335e5cfb3762b423356b8c477fa9b737e35d4
glebasson <glebasson@yandex.ru>
2018-10-30 19:44:08 +0300

    mngs added

--- coord_descent.py
+++ coord_descent.py
@@ -15,7 +15,14 @@ def main():
 
     x_min = coord_descent(A, b, np.array([1, 0, 0]))
     f_min = quadraticf(x_min, A, b, C)
+    print("Via coordinate method:")
     print("Minimum X: %s\nFunction min value: %s" % (x_min, f_min))
+
+    x_min = mngs(A, b, np.array([1, 0, 0]))
+    f_min = quadraticf(x_min, A, b, C)
+    print("Via mngs:")
+    print("Minimum X: %s\nFunction min value: %s" % (x_min, f_min))
+
 
 
 
@@ -31,6 +38,27 @@ def coord_descent(A, b, x1):
         e[np.random.randint(0, 3)] = 1
         # Evalute step
         mu = - (np.dot(e, grad(x1))) / np.dot(e, np.dot(A, e))
+        # Make new step
+        xk = x1
+        x1 = x1 + mu * e
+        # Break condition
+        if np.linalg.norm(grad(x1)) < 1e-6:
+            break
+
+    return x1
+
+
+def mngs(A, b, x1):
+    # Starting position
+    xk = x1
+    grad = lambda x: np.dot(A, x) + b
+
+    while True:
+        # Random descent vecotor
+        e = grad(x1)
+        # Evalute step
+        mu = - (np.linalg.norm(grad(x1) ** 2)) / np.dot(np.dot(grad(x1), A),
+                                                        grad(x1))
         # Make new step
         xk = x1
         x1 = x1 + mu * e

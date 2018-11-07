b1f35249bdcab6d3bab15c9f46193f75f9894b59
glebasson <glebasson@yandex.ru>
2018-11-07 10:50:07 +0300

    poly plot fixed

--- source/interpolation.py
+++ source/interpolation.py
@@ -1,21 +1,24 @@
 """
 Function interpolation
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def main():
     # number of points and [a, b] interval
-    N = 5
+    N = 10
     a, b = -2, 2
-    x, pol = interp(f, a, b, N)
+    pol = interp(f, a, b, N)
+    x = np.arange(-2, 2, 0.01)
 
     # plot results
     x_f = np.arange(a, b, 0.01)
+    plt.subplot(211)
     plt.plot(x_f, f(x_f))
     y = [evalpol(p, pol) for p in x]
+
+    plt.subplot(212)
     plt.plot(x, y, color="green")
     plt.show()
 
@@ -37,7 +40,7 @@ def interp(func, a, b, N):
     A = np.array([[p ** n for n in reversed(range(0, N))] for p in x])
     B = np.array([func(y) for y in x])
     pol = np.linalg.solve(A, B)
-    return x, pol
+    return pol
 
 
 if __name__ == "__main__":
